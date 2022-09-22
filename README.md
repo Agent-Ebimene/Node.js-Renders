a83df4f534aa32c635c5bcc3bf12aa9cf0068a8c
Agent Ebimene <ebimeneagent@gmail.com>
2022-09-22 14:19:42 +0100

    Revisit the express framework for basic servers

---
+++ README.md
@@ -0,0 +1,3 @@
+### Exploring Node js to accomplish my fullstack dream.
+
+#### Revisited the express framework for creating basic servers that return resources when a user searches the url
--- index.js
+++ index.js
@@ -1,66 +1,19 @@
+console.log("Ye!!");
 const express = require("express");
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening at ${PORT}`);
-});
-
-app.use(express.static("./public"));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/about.html");
+  console.log("User hit the resource");
+  res.status(200).send("Home Page");
 });
 
-//Creating a file with the appendFile method
-fs.appendFile("my-name.txt", "Agent Ebimene", (err) => {
-  if (err) throw err;
-  console.log("Saved");
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
 });
-fs.open("my-surname.txt", "w", (err, file) => {
-  console.log("saved");
+
+app.all("*", (req, res) => {
+  res.status(404).send(`<h1>Resource not found</h1>`);
 });
-fs.writeFile("my-surname.txt", "Engineer Agent Eli", (err) => {
-  if (!err) console.log("Saved");
+app.listen(5000, () => {
+  console.log(`App is listening at port ${5000}`);
 });
-const fs = require("fs");
-const http = require("http");
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(req.url);
-    res.end();
-  })
-  .listen(3000);
-
-// Using the module to render different files on the same folder
-
-const url = require("url");
-const fs = require("fs");
-const http = require("http");
-http
-  .createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const file = "." + q.pathname;
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        res.writeHead(404, { "content-type": "text/html" });
-        return res.end("404 page!");
-      }
-      res.writeHead(200, { "content-type": "text/html" });
-      console.log(file);
-      res.write(data);
-    });
-  })
-  .listen(5000);
-
-// Exploring different modules
-
-const uc = require("upper-case");
-const http = require("http");
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(uc.upperCase("hello world"));
-    res.end();
-  })
-  .listen(5000);
